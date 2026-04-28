ae775d81c58345ecb217205fef196531a47fabcf
bimaghafara8-boop <bimaghafara8@gmail.com>
2026-04-28 16:36:39 +0700

    next perbaikan lagi

--- boot.asm
+++ boot.asm
@@ -1,23 +1,79 @@
 [bits 16]
-[org 0x7c00]
-start:
-  mov si, pesan
-  mov ah, 0x0e
+global mulai
 
-loop_teks:
-  lodsb
-  cmp al, 0
-  je stop
-  int 0x10
+mulai:
+  cli
+  mov [boot_drive], dl
 
-  jmp loop_teks
+  xor ax, ax
+  mov ds, ax
+  mov es, ax
+  mov ss, ax
+  mov sp, 0x7c00
 
-stop:
+  mov ah, 0x02
+  mov al, 1
+  mov ch, 0
+  mov dh, 0
+  mov cl, 2
+  mov bx, 0x7e00
+
+  mov dl, [boot_drive]
+  int 0x13
+  jc $
+
+  in al, 0x92
+  or al, 0x2
+  out 0x92, al
+
+  mov eax, cr0
+  or eax, 0x1
+  mov cr0, eax
+
+  jmp 0x08:mulai_32bit
+
+boot_drive db 0
+
+[bits 32]
+mulai_32bit:
+  mov ax, 0x10
+  mov ds, ax
+  mov ss, ax
+  mov es, ax
+  mov fs, ax
+  mov gs, ax
+
+  mov ebp, 0x90000
+  mov esp, ebp
+
+  extern main_c
+  call main_c
   jmp $
 
-pesan:
-  db "sistem ready!!", 0
+gdt_mulai:
+  dq 0x0
+
+gdt_kode:
+  dw 0xffff
+  dw 0x0
+  db 0x0
+  db 10011010b
+  db 11001111b
+  db 0x0
+
+gdt_data:
+  dw 0xffff
+  dw 0x0
+  db 0x0
+  db 10010010b
+  db 11001111b
+  db 0x0
+
+gdt_selesai:
+
+gdt_tabel:
+  dw gdt_selesai - gdt_mulai - 1
+  dd gdt_mulai
 
 times 510 - ($ - $$) db 0
 dw 0xaa55
-
